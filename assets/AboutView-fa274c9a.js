5a036342b30e523309484a1f5045d8c2c85cee44
King098 <singlewalker098@gmail.com>
2023-04-15 14:54:49 +0800

    test

---
+++ assets/AboutView-fa274c9a.js
@@ -0,0 +1,1 @@
+import{h as e,o as t,c as o,i as s}from"./index-184b95fa.js";const _={},c={class:"about"},n=s("h1",null,"This is an about page",-1),a=[n];function i(r,u){return t(),o("div",c,a)}const l=e(_,[["render",i]]);export{l as default};
