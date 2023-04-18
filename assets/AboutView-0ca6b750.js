d208fb78d622d18b323f4545abf0016cee8a955e
King098 <singlewalker098@gmail.com>
2023-04-18 20:36:57 +0800

    update

--- assets/AboutView-0ca6b750.js
+++
@@ -1,1 +0,0 @@
-import{f as e,o as t,c as o,g as s}from"./index-8447c799.js";const _={},c={class:"about"},n=s("h1",null,"This is an about page",-1),a=[n];function i(r,u){return t(),o("div",c,a)}const l=e(_,[["render",i]]);export{l as default};
